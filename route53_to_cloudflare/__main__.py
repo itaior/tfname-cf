6f7d2bf0b057421da0507c85f3012ff89117b868
Guy Saar <guy.saar@tikalk.com>
2022-02-17 17:36:12 +0200

    removed duplicate writing to records.tf

--- route53_to_cloudflare/__main__.py
+++ route53_to_cloudflare/__main__.py
@@ -547,21 +547,6 @@ def render(zone, rs, zoneName, account_id, cloudflare_ns_record, aws_account_id)
             
             # delete the records file
             os.remove(f'./{aws_account_id}/{zoneName}/{item}.tf')
-    
-    # this loop will right the records to one file orderd by record type 
-    for item in resources:
-        # create file only for the necessary records
-        if not len(resources[item]) == 0:
-            # Read in the file
-            with open(f'./{aws_account_id}/{zoneName}/{item}.tf', 'r') as file :
-                filedata = file.read()
-            
-            # Write the file out again
-            with open(f"./{aws_account_id}/{zoneName}/records.tf", 'a') as file:
-                file.write(filedata)
-            
-            # delete the records file
-            os.remove(f'./{aws_account_id}/{zoneName}/{item}.tf')
 
 def main():
     # get input parameters
